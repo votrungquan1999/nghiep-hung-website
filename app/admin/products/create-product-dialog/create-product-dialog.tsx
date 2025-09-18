4f0bbbcb6f4474512b313b1c1230d639ae3591af
Quan Vo <votrungquan99@gmail.com>
2025-09-18 09:54:30 +0700

    refactor: update admin product creation with enhanced form state
    - Update create product dialog to use enhanced form state system
    - Integrate FormBoundaryProvider for proper form reset
    - Maintain server component architecture with proper composition
    - Enhance dialog structure for better form management

--- app/admin/products/create-product-dialog/create-product-dialog.tsx
+++ app/admin/products/create-product-dialog/create-product-dialog.tsx
@@ -1,14 +1,15 @@
-import { Plus } from "lucide-react"
-import { Button } from "@/components/ui/button"
+import { Plus } from "lucide-react";
+import { FormBoundaryProvider } from "@/components/form-state/form-state.state";
+import { Button } from "@/components/ui/button";
 import {
 	Dialog,
 	DialogContent,
 	DialogHeader,
 	DialogTitle,
 	DialogTrigger,
-} from "@/components/ui/dialog"
-import { ProductImageSelectionProvider } from "./create-product-dialog.state"
-import { CreateProductForm } from "./create-product-form"
+} from "@/components/ui/dialog";
+import { ProductImageSelectionProvider } from "./create-product-dialog.state";
+import { CreateProductForm } from "./create-product-form";
 
 /**
  * Create product dialog server component
@@ -18,21 +19,23 @@ import { CreateProductForm } from "./create-product-form"
 export default function CreateProductDialog() {
 	return (
 		<ProductImageSelectionProvider>
-			<Dialog>
-				<DialogTrigger asChild>
-					<Button>
-						<Plus className="mr-2 size-4" />
-						Add New Product
-					</Button>
-				</DialogTrigger>
-				<DialogContent className="max-w-4xl max-h-[90vh] overflow-y-auto bg-white">
-					<DialogHeader>
-						<DialogTitle>Create New Product</DialogTitle>
-					</DialogHeader>
+			<FormBoundaryProvider>
+				<Dialog>
+					<DialogTrigger asChild>
+						<Button>
+							<Plus className="mr-2 size-4" />
+							Add New Product
+						</Button>
+					</DialogTrigger>
+					<DialogContent className="max-w-4xl max-h-[90vh] overflow-y-auto bg-white">
+						<DialogHeader>
+							<DialogTitle>Create New Product</DialogTitle>
+						</DialogHeader>
 
-					<CreateProductForm />
-				</DialogContent>
-			</Dialog>
+						<CreateProductForm />
+					</DialogContent>
+				</Dialog>
+			</FormBoundaryProvider>
 		</ProductImageSelectionProvider>
-	)
+	);
 }
